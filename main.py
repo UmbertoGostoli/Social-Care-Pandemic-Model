76688e881b8396aa2c90125f9ddbd7a006693d33
UmbertoGostoli <35143912+UmbertoGostoli@users.noreply.github.com>
2021-02-08 13:37:31 +0000

    Update main.py

--- main.py
+++ main.py
@@ -754,7 +754,7 @@ if __name__ == "__main__":
         graphsDummy.to_csv("graphsParams.csv", index=False)
         
     
-    parametersFromFiles = False
+    parametersFromFiles = True
     
     scenariosParams = []
     policiesParams = [[[]]]
